6e18c42271adfacbe9bac15d0a98007e38a74915
ghala <ghala.swe@outlook.com>
2026-09-19 19:03:15 +0300

    Add files via upload

---
+++ src/main/java/com/student/Calculator.java
@@ -0,0 +1,13 @@
+
+package com.student;
+
+public class Calculator implements CalculatorOperations {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/main/java/com/student/CalculatorOperations.java
@@ -0,0 +1,8 @@
+package com.student;
+
+public interface CalculatorOperations {
+
+    int add(int a, int b);
+
+    int subtract(int a, int b);
+}
